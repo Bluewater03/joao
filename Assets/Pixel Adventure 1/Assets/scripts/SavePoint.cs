5cc12b69bb15cca0a0bb8170663808086516ba9a
Bluewater03 <aguamaisazul@gmail.com>
2022-05-26 17:03:18 -0300

    projeto salvo

---
+++ Assets/Pixel Adventure 1/Assets/scripts/SavePoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SavePoint : MonoBehaviour
+{
+    public int totalPontos;
+    public Text contador;
+    
+    public GameObject gameOver;
+    
+    public static SavePoint acesso;
+
+    void Start()
+    {
+        acesso = this;
+    
+    }
+
+    public void atualizaPontosText() => contador.text = totalPontos.
+    ToString();
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+}
---
+++ Assets/Pixel Adventure 1/Assets/scripts/frutinha.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frutinha : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private CircleCollider2D collider;
+    
+    public GameObject coletado;
+    
+    public int Pontos;
+    
+    // Start is called before the first frame update
+     void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<CircleCollider2D>();    
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+        sprite.enabled = false;
+        collider.enabled = false;
+
+        coletado.SetActive(true);
+        
+        SavePoint.acesso.totalPontos += Pontos;
+
+        SavePoint.acesso.atualizaPontosText();
+
+        Destroy(gameObject, 0.4f);
+        
+        }
+    
+    
+    
+    
+    
+    }
+}
